20f9eb0dd7466a1aa8ab1492a8669ecdd50fe53a
SantiagoChaparro <41376467+SantiagoChaparro@users.noreply.github.com>
2023-07-10 20:55:36 -0500

    fix trench button spawn time

--- Assets/Scripts/UnitsBehaviours/TrenchButton.cs
+++ Assets/Scripts/UnitsBehaviours/TrenchButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TrenchButton : MonoBehaviour
 {
@@ -8,6 +9,17 @@ public class TrenchButton : MonoBehaviour
     [SerializeField] private GameObject trenchButtonObject;
 
     [SerializeField] private GameObject trench;
+
+    [SerializeField] private float secondsBeforeAppearingForTheFirstTime;
+
+    private DateTime activationTime;
+
+    private bool shouldActivate = false;
+
+    private void Awake()
+    {
+        activationTime = DateTime.Now.AddSeconds(secondsBeforeAppearingForTheFirstTime);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +29,24 @@ public class TrenchButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (shouldActivate && !trenchButtonObject.active && DateTime.Now > activationTime)
+        {
+            TrenchButtonManager.Instance.SetCurrentSelectedUnit(trenchButtonObject);
+            shouldActivate = false;
+        }
     }
 
     private void OnMouseEnter()
     {
         if(!trench.active)
         {
-            TrenchButtonManager.Instance.SetCurrentSelectedUnit(trenchButtonObject);
+           shouldActivate = true;
         }
+    }
+
+    private void OnMouseExit()
+    {
+        shouldActivate = false;
     }
 
     public void onClick()
