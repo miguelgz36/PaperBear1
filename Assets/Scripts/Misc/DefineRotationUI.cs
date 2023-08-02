f9cccaded33635a9a86f6bbdcb73b9e26c80e01e
Miguel Angel Beltrán Rodríguez <mmiguel360360@outlook.com>
2023-08-01 23:24:37 -0500

    added health bar

---
+++ Assets/Scripts/Misc/DefineRotationUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefineRotationUI : MonoBehaviour
+{
+    [SerializeField] UnitController unitController;
+
+
+    void LateUpdate()
+    {
+        if (transform.rotation.eulerAngles.z >= -90 && transform.rotation.eulerAngles.z < 90)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        } 
+        else if(transform.rotation.eulerAngles.z >= 90 && transform.rotation.eulerAngles.z <= 270)
+        {
+            transform.localScale = new Vector3(-1, -1, 1);
+        }
+        transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+    }
+}
