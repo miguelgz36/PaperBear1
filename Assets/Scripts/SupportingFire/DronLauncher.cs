80c11190d031dcae79a78700a614986b841dbd47
Miguel Angel Beltrán Rodríguez <mmiguel360360@outlook.com>
2023-10-22 16:13:17 -0500

    fix bug place IA and smoke fire support

--- Assets/Scripts/SupportingFire/DronLauncher.cs
+++ Assets/Scripts/SupportingFire/DronLauncher.cs
@@ -6,9 +6,11 @@ public class DronLauncher : Placeable
 {
 
     [SerializeField] GameObject dron;
+    [SerializeField] Transform particles;
 
     public void DeployDron(Vector3 target, Vector3 origin)
     {
+        particles.position = target;
         GameObject dronInstance = Instantiate(this.dron, origin, Quaternion.identity);
         Dron dron = dronInstance.GetComponent<Dron>();
         dron.Deploy(target, origin);
